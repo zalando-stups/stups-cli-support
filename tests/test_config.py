921e09c20bd5dc3b0f68d8644ffa9b9f51eb5e0e
Henning Jacobs <henning@jacobs1.de>
2015-12-09 12:21:16 +0100

    #3 add test for env vars

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import os
 import stups_cli.config
 
 
@@ -11,3 +12,10 @@ def test_store_load_config():
     stups_cli.config.store_config(cfg, 'foobar')
     new_cfg = stups_cli.config.load_config('foobar')
     assert cfg == new_cfg
+
+
+def test_load_config_with_environment():
+    os.environ['EXAMPLE_PROJ_MY_KEY'] = 'some value'
+    cfg = stups_cli.config.load_config('example-proj')
+    assert cfg == {'my_key': 'some value'}
+
