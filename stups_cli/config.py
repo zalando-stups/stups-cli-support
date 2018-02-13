bc7de3bc3288422094db64b3abaf25f1d24e9085
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2018-02-13 22:35:59 +0100

    No error message when no domain is given (#15)

--- stups_cli/config.py
+++ stups_cli/config.py
@@ -66,10 +66,11 @@ def configure(preselected_domain=None):
         domain = preselected_domain or existing_config.get('domain')
 
         while True:
-            if is_valid_domain(domain):
-                break
-            else:
-                info('The entered domain is not valid. Please try again.')
+            if domain:
+                if is_valid_domain(domain):
+                    break
+                else:
+                    info('The entered domain is not valid. Please try again.')
             domain = click.prompt('Please enter your STUPS domain (e.g. "stups.example.org")', default=domain)
 
         for component in ('zalando-token-cli', 'zalando-aws-cli', 'zalando-kubectl', 'zalando-deploy-cli'):
